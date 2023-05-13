01b4324732eaf02da67364d9ecd58de2c57e57e2
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-05-13 23:52:17 +0200

    Add button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function generateGrid() {
-  const container = document.getElementById("container-main");
+  const container = document.getElementById("container-sketch");
   for (let i = 0; i < 16; i++) {
     const boxRow = document.createElement("div");
     container.appendChild(boxRow);
@@ -8,7 +8,6 @@ function generateGrid() {
       const box = document.createElement("div");
       boxRow.appendChild(box);
       box.classList.add("box");
-      //   box.textContent = "x";
     }
   }
 }
